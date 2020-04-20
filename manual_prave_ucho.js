f7e015ca9908b9fedc442a1b5ee9bb2dcddd2caf
Lukáš Hudák <lukas.hudak.4@student.tuke.sk>
2020-04-20 15:01:55 +0200

    zmeny_obrazkyrobot_home_button_lave_prave_spolocny_priecinok

--- manual_prave_ucho.js
+++ manual_prave_ucho.js
@@ -30,7 +30,7 @@ function playFunc(target, RepeatCount) {
 function main(){
    
     playbtn.addEventListener('click', function() {
-            var audio = new Audio('audio/1000Hz.wav');
+            var audio = new Audio('audio_prave_ucho/1000Hz.wav');
             audio.play();
             playFunc(audio, 1);
             
