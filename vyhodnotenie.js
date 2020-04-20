d14b219eb8b5dafbe94ac08fc55be0829e795a96
Lukas Hudak <lukas.hudak.4@student.tuke.sk>
2020-04-20 13:15:11 +0200

    oddelene_testovanie_usi

--- vyhodnotenie.js
+++ vyhodnotenie.js
@@ -101,6 +101,26 @@ function confirmation() {
 }
 
 
+function download(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + 
+  
+  encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+  
+    pom.style.display = 'none';
+    document.body.appendChild(pom);
+  
+    pom.click();
+  
+    document.body.removeChild(pom);
+}
+
+
+
+
+
+
 
 function main(){
     myFunction();
